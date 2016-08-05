449f595a9cf035dd7f93bf97b790d8d36568e74c
Jacob.Vertouso <jacob.vertouso.lpg@gmail.com>
2016-08-05 16:49:22 +0800

    * added pause button, pause menu, resume

---
+++ Assets/Scripts/ResumeScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResumeScript : MonoBehaviour {
+
+	public GameObject pauseMenu;
+	// Use this for initialization
+	public void onClick(){
+		pauseMenu.SetActive (false);
+		Time.timeScale = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

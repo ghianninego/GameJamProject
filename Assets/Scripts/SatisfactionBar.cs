8b36eebd156ca15b94ae3a492c153fab49fe8f62
Jacob.Vertouso <jacob.vertouso.lpg@gmail.com>
2016-08-09 11:56:51 +0800

    **

---
+++ Assets/Scripts/SatisfactionBar.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SatisfactionBar : MonoBehaviour {
+
+	public int startingSatisfaction = 100;
+	public int currentSatisfaction;
+
+
+	// Use this for initialization
+	void awake () {
+
+		currentSatisfaction = startingSatisfaction;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	
+	}
+}

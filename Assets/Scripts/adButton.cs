e20fbdcd39391067377d15936d6583eff8af16d2
Jacob.Vertouso <jacob.vertouso.lpg@gmail.com>
2016-08-11 18:19:51 +0800

    **

---
+++ Assets/Scripts/adButton.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class adButton : MonoBehaviour {
+
+	public void onClick(){
+		SceneManager.LoadScene (2);
+	}
+
+}
--- Assets/Scripts/retryButton.cs
+++ Assets/Scripts/retryButton.cs
@@ -8,7 +8,10 @@ public class retryButton : MonoBehaviour {
 
 	public void onClick(){
 		SuccessPanel.SetActive (false);
+		Time.timeScale = 1;
 		SceneManager.LoadScene (1);
+
+		//SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex);
 		//Application.LoadLevel(Application.loadedLevel);
 		
 	}

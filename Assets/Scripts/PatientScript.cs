a98f2a8d652a91466067b5a36726894f3b09c237
Ghiannine Shiarra J. Go <ghianninegolp@gmail.com>
2016-08-11 14:13:38 +0800

    * Added comments on some scripts - removed ScoreScript.cs

--- Assets/Scripts/PatientScript.cs
+++ Assets/Scripts/PatientScript.cs
@@ -1,15 +1,24 @@
-﻿using UnityEngine;
+﻿/* PatientScript.cs
+ * Author: Ghiannine Go
+ * 
+ * This script manages a certain patient, whether it is a girl, boy or a teacher, and whether if this patient
+ * has already been served or not. This will determine the patient's satisfaction rate.
+ * 
+ * */
+
+using UnityEngine;
 using System.Collections;
 
 public class PatientScript : MonoBehaviour {
 
-	//time a new patient appears
-	public float timeAppear = 5f;
+	#region variables
 	public UIWidget concernBG;
 	private int patienceLevel;
 	private int currentPatience;
 	private bool served = false;
+	#endregion
 
+	#region MonoBehaviour
 	void Start () {
 		patienceLevel = PlayerManager.Instance.SetPatienceLevel (gameObject.tag);
 		currentPatience = patienceLevel;
@@ -17,12 +26,25 @@ public class PatientScript : MonoBehaviour {
 
 		StartCoroutine("NotYetServed");
 	}
+	#endregion
 
+
+	/* This function is called whenever the selected patient gets served.
+	 * It adds the achieved satisfaction rate for that patient to the current score
+	 *
+	 * param: none
+	 * return: none
+	 */
 	void Served() {
 		served = true;
 		GameManager.Instance.SetScore (currentPatience/patienceLevel);
 	}
 
+	/* This function changes the color of the patient's mood based on his/her patience level.
+	 *
+	 * param: none
+	 * return: none
+	 */
 	void ChangeColor() {
 		if (currentPatience == (int)(0.25 * patienceLevel))
 			concernBG.color = Color.red;
@@ -32,6 +54,12 @@ public class PatientScript : MonoBehaviour {
 			concernBG.color = Color.yellow;
 	}
 
+	/* This function will run until the patient gets served.
+	 * The patient will leave once his/her patience reaches 0.
+	 *
+	 * param: none
+	 * return: none
+	 */
 	IEnumerator NotYetServed() {
 		while (served == false) {
 			yield return new WaitForSeconds (1.0f);
